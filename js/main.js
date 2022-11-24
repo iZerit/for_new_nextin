04d638bcf8b7dc80ac2bca8fcf799a1165485985
Ibrokhim <izerit@mail.ru>
2022-11-24 13:14:35 -0700

    map button

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@ var phoneMask = IMask(
     document.getElementById('phone-mask'), {
       mask: '+{998}(00)000-00-00'
     });
+
+
+
+    function buttonClicked() {
+        const hideDiv = document.getElementById('map_divv')
+        if(hideDiv.style.display == 'none'){
+            $("#map_divv").show();
+        }else{
+            $("#map_divv").hide();
+
+        }
+        }
