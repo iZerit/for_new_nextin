253800c194cc79aef062bf38c8d075d2b147407f
Ibrokhim <izerit@mail.ru>
2022-11-25 14:25:05 -0700

    scrool top and down edited

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ var phoneMask = IMask(
             $("#hide_div_but").show();
         });
 
-        
+
         $("#hide_div_but").click(function(){
             $("#show_div_but").show();
             $("#map_divv").hide();
@@ -25,3 +25,38 @@ var phoneMask = IMask(
 
         
       });
+
+
+
+
+
+
+
+
+
+
+      //Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 40 ||
+document.documentElement.scrollTop > 40
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
