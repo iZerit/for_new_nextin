85a57e174e10c23b3388053ca5ac648cba4b37e4
Ibrokhim <izerit@mail.ru>
2022-11-25 16:10:51 -0700

    nothing:)

--- js/main.js
+++ js/main.js
@@ -27,10 +27,42 @@ var phoneMask = IMask(
       });
 
 
+// // scrool top vvvvvvvv
+// let mybutton = document.getElementById("btn-back-to-top");
+
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function () {
+// scrollFunction();
+// };
+
+// function scrollFunction() {
+// if (
+// document.body.scrollTop > 20 ||
+// document.documentElement.scrollTop > 20
+// ) {
+// mybutton.style.display = "block";
+// } else {
+// mybutton.style.display = "none";
+// }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// mybutton.addEventListener("click", backToTop);
+
+// function backToTop() {
+// document.body.scrollTop = 0;
+// document.documentElement.scrollTop = 0;
+// }
+
+// // scrool top  ^^^^^ 
 
 
 
-// for scrool bottom invisible 
+
+
+
+
+
+// for scrool bottom invincible 
 
 $(document).ready(function(){
 
@@ -42,11 +74,15 @@ $(document).ready(function(){
           $('#scrool_bottom').show();
       }
 
-// for scrool bottom invisible 
+// for scrool bottom invincible 
 
+
+
+
+// for scrool top vvvvvvvvv
 
       $(window).scroll(function(){
-        if(this.scrollY > 300){
+        if(this.scrollY > 50){
           $('#btn-back-to-top').show();
         }else{
           $('#btn-back-to-top').hide();
@@ -54,11 +90,18 @@ $(document).ready(function(){
       })
 
 
+
       $("#btn-back-to-top").click(function(){
         $(window).scrollTop(0);
       })
 
-  })
+
+
+
+      // for scrool top ^^^^^^^^^^
+
+
+})
 
 
 
